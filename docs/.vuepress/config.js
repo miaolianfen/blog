5991178c13479361692443f142a59b19180820cc
miaolianfen <miaolianfen@corp.netease.com>
2020-11-14 23:17:05 +0800

    feat:初步完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,66 @@
 module.exports = {
-    title: '风儿的天地',
-    description: 'Just playing around',
-    base: '/blog/'
+    title: '风之博客',
+    description: '做一个内外兼修的程序员',
+    base: '/blog/',
+    themeConfig: {
+      // logo: '/public/img/logo.png',
+      nav: [
+        { text: '主页', link: '/' },
+        { text: '总结', link: '/bugfree/' },
+        { text: '读书', link: '/book/' },
+        { text: '刷题', link: '/leetcode/' },
+        
+      ],
+      sidebar: 'auto'
+      // sidebar: {
+      //   '/home/book': [
+      //     '',     /* /foo/ */
+      //     'three'
+      //   ],
+      //   '/bugfree/': [
+      //     '',     /* /foo/ */
+      //     'one',  /* /foo/one.html */
+      //     'two'   /* /foo/two.html */
+      //   ],
+  
+      //   '/book/': [
+      //     '',      /* /bar/ */
+      //     'three', /* /bar/three.html */
+      //   ]
+      // }
+
+      // sidebar:[
+      //  {
+      //     title: '总结->bugfree',   // 必要的
+      //     path: '/bugfree/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+      //     collapsable: false, // 可选的, 默认值是 true,
+      //     sidebarDepth: 1,    // 可选的, 默认值是 1
+      //     children: [
+      //       '',
+      //       'one.md',
+      //       'two.md'
+      //     ]
+      //   },
+      //   {
+      //     title: '读书->内外兼修',
+      //     path: '/book/', 
+      //     collapsable: false, // 可选的, 默认值是 true,
+      //     sidebarDepth: 1,    // 可选的, 默认值是 1
+      //     children: [
+      //       '/'
+      //     ]
+      //   },
+      //   {
+      //     title: '刷题->保持思考',
+      //     path:'/leetcode/',
+      //     collapsable: false, // 可选的, 默认值是 true,
+      //     sidebarDepth: 1,    // 可选的, 默认值是 1
+      //     children: [
+      //       '/'
+      //     ]
+      //   }
+      // ]
+    },
+    
+    
   }
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+# heroImage: /hero.png
+heroText: 风儿的移动城堡
+tagline: 且行且珍惜
+actionText: 快速上手 →
+actionLink: /home/
+features:
+- title: 封装
+  details: 永远别小看程序媛
+- title: 继承
+  details: 程序员只有一种死法：土死的
+- title: 多态
+  details: 0.1+0.2=0.3？
+footer: MIT Licensed | Copyright © 2020-present 风儿(miaolianfen)
+---
